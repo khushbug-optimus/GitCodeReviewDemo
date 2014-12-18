b803595ba8ddaf49f23fa85ff30d584a4d51ba0c
khushbu <khushbu.garg@optimusinfo.com>
2014-12-18 13:14:50 +0530

    Demo project for code review

---
+++ CodeReviewDemo/GitCodeReviewDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCodeReviewDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 34;
+            int b = 45;
+
+            int c = a + b;
+
+            Console.WriteLine("Sum:" + c);
+
+
+            int d = b - a;
+            Console.WriteLine("difference:" + d);
+
+            Console.ReadLine();
+        }
+    }
+}
